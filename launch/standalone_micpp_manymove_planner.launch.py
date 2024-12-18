9ab69879f45df10a7b199e58969079eb5f378129
pastoriomarco <pastoriomarco@gmail.com>
2024-12-18 10:04:20 +0100

    added standalone demo and launcher, working for lite6, to update

--- launch/standalone_micpp_manymove_planner.launch.py
+++ launch/standalone_micpp_manymove_planner.launch.py
@@ -1,227 +1,231 @@
 import os
-from launch.actions import OpaqueFunction, DeclareLaunchArgument
-from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.launch_context import LaunchContext
+from ament_index_python.packages import get_package_share_directory
 from uf_ros_lib.moveit_configs_builder import MoveItConfigsBuilder
-from uf_ros_lib.uf_robot_utils import generate_ros2_control_params_temp_file
 
-import yaml
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import OpaqueFunction
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
-from launch.conditions import IfCondition
-from launch_ros.substitutions import FindPackageShare
-from launch.actions import RegisterEventHandler, EmitEvent
-from launch.event_handlers import OnProcessExit
-from launch.events import Shutdown
 
-def launch_setup(context, *args, **kwargs):
-    dof = LaunchConfiguration('dof', default=6)
-    robot_type = LaunchConfiguration('robot_type', default='lite')
-    prefix = LaunchConfiguration('prefix', default='')
-    hw_ns = LaunchConfiguration('hw_ns', default='ufactory')
-    limited = LaunchConfiguration('limited', default=True)
-    effort_control = LaunchConfiguration('effort_control', default=False)
-    velocity_control = LaunchConfiguration('velocity_control', default=False)
-    model1300 = LaunchConfiguration('model1300', default=False)
-    robot_sn = LaunchConfiguration('robot_sn', default='')
-    attach_to = LaunchConfiguration('attach_to', default='world')
-    attach_xyz = LaunchConfiguration('attach_xyz', default='"0 0 0"')
-    attach_rpy = LaunchConfiguration('attach_rpy', default='"0 0 0"')
-    mesh_suffix = LaunchConfiguration('mesh_suffix', default='stl')
-    kinematics_suffix = LaunchConfiguration('kinematics_suffix', default='')
+def generate_launch_description():
 
-    add_gripper = LaunchConfiguration('add_gripper', default=False)
-    add_vacuum_gripper = LaunchConfiguration('add_vacuum_gripper', default=False)
-    add_bio_gripper = LaunchConfiguration('add_bio_gripper', default=False)
-    add_realsense_d435i = LaunchConfiguration('add_realsense_d435i', default=False)
-    add_d435i_links = LaunchConfiguration('add_d435i_links', default=True)
-    add_other_geometry = LaunchConfiguration('add_other_geometry', default=True)
-    geometry_type = LaunchConfiguration('geometry_type', default='mesh')
-    geometry_mass = LaunchConfiguration('geometry_mass', default=0.3)
-    geometry_height = LaunchConfiguration('geometry_height', default=0.1)
-    geometry_radius = LaunchConfiguration('geometry_radius', default=0.1)
-    geometry_length = LaunchConfiguration('geometry_length', default=0.1)
-    geometry_width = LaunchConfiguration('geometry_width', default=0.1)
-    geometry_mesh_filename = LaunchConfiguration('geometry_mesh_filename', default='pneumatic_lite.stl')
-    geometry_mesh_origin_xyz = LaunchConfiguration('geometry_mesh_origin_xyz', default='"0 0 0"')
-    geometry_mesh_origin_rpy = LaunchConfiguration('geometry_mesh_origin_rpy', default='"0 0 0"')
-    geometry_mesh_tcp_xyz = LaunchConfiguration('geometry_mesh_tcp_xyz', default='"0.03075 0 0.11885"')
-    geometry_mesh_tcp_rpy = LaunchConfiguration('geometry_mesh_tcp_rpy', default='"0 0.52 0"')
-
-    no_gui_ctrl = LaunchConfiguration('no_gui_ctrl', default=False)
-    ros_namespace = LaunchConfiguration('ros_namespace', default='').perform(context)
-    show_rviz = LaunchConfiguration('show_rviz', default=True)
-    use_sim_time = LaunchConfiguration('use_sim_time', default=False)
-    moveit_config_package_name = 'xarm_moveit_config'
-
-    # Retrieve your custom parameters
-    velocity_scaling_factor = LaunchConfiguration('velocity_scaling_factor')
-    acceleration_scaling_factor = LaunchConfiguration('acceleration_scaling_factor')
-    max_exec_retries = LaunchConfiguration('max_exec_retries')
-    smoothing_type = LaunchConfiguration('smoothing_type')
-    step_size = LaunchConfiguration('step_size')
-    jump_threshold = LaunchConfiguration('jump_threshold')
-    max_cartesian_speed = LaunchConfiguration('max_cartesian_speed')
-    plan_number_target = LaunchConfiguration('plan_number_target')
-    plan_number_limit = LaunchConfiguration('plan_number_limit')
-
-    ros2_control_plugin = 'uf_robot_hardware/UFRobotFakeSystemHardware'
-    controllers_name = 'fake_controllers'
-    xarm_type = '{}{}'.format(robot_type.perform(context), dof.perform(context) if robot_type.perform(context) in ('xarm', 'lite') else '')
-
-    ros2_control_params = generate_ros2_control_params_temp_file(
-        os.path.join(get_package_share_directory('xarm_controller'), 'config', '{}_controllers.yaml'.format(xarm_type)),
-        prefix=prefix.perform(context), 
-        add_gripper=add_gripper.perform(context) in ('True', 'true'),
-        add_bio_gripper=add_bio_gripper.perform(context) in ('True', 'true'),
-        ros_namespace=ros_namespace,
-        robot_type=robot_type.perform(context)
+    # Load the robot configuration
+    moveit_config = (
+        MoveItConfigsBuilder(
+            controllers_name="fake_controllers",
+            ros2_control_plugin="uf_robot_hardware/UFRobotFakeSystemHardware",
+            context=LaunchContext(),
+            robot_type="lite",
+            dof=6, 
+            add_realsense_d435i=True, 
+            add_d435i_links=True,
+            add_other_geometry=True,
+            geometry_type="mesh",
+            geometry_mass=0.3,
+            geometry_mesh_filename="pneumatic_lite.stl",
+            geometry_mesh_tcp_xyz="0.03075 0 0.11885",
+            geometry_mesh_tcp_rpy="0 0.52 0",
+            kinematics_suffix="LS1"
+        )
+        .robot_description()
+        .trajectory_execution(file_path="config/lite6/fake_controllers.yaml")
+        .planning_scene_monitor(
+            publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .planning_pipelines(pipelines=["ompl"])
+        .moveit_cpp(file_path=get_package_share_directory("micpp_manymove_planner") + "/config/moveit_cpp.yaml")
+        .to_moveit_configs()
     )
 
-    # moveit_configs.robot_description
-    # moveit_configs.robot_description_semantic
-    # moveit_configs.robot_description_kinematics
-    # moveit_configs.planning_pipelines
-    # moveit_configs.trajectory_execution
-    # moveit_configs.planning_scene_monitor
-    # moveit_configs.joint_limits
-    # moveit_configs.to_dict()
+    run_move_group_node = Node(
+        package="micpp_manymove_planner",
+        executable="standalone_micpp_demo",
+        output="screen",
+        parameters=[moveit_config.to_dict()],
+        arguments=["--log-level", "debug"],
+    )
 
-    moveit_configs = MoveItConfigsBuilder(
-        context=context,
-        controllers_name=controllers_name,
-        dof=dof,
-        robot_type=robot_type,
-        prefix=prefix,
-        hw_ns=hw_ns,
-        limited=limited,
-        effort_control=effort_control,
-        velocity_control=velocity_control,
-        model1300=model1300,
-        robot_sn=robot_sn,
-        attach_to=attach_to,
-        attach_xyz=attach_xyz,
-        attach_rpy=attach_rpy,
-        mesh_suffix=mesh_suffix,
-        kinematics_suffix=kinematics_suffix,
-        ros2_control_plugin=ros2_control_plugin,
-        ros2_control_params=ros2_control_params,
-        add_gripper=add_gripper,
-        add_vacuum_gripper=add_vacuum_gripper,
-        add_bio_gripper=add_bio_gripper,
-        add_realsense_d435i=add_realsense_d435i,
-        add_d435i_links=add_d435i_links,
-        add_other_geometry=add_other_geometry,
-        geometry_type=geometry_type,
-        geometry_mass=geometry_mass,
-        geometry_height=geometry_height,
-        geometry_radius=geometry_radius,
-        geometry_length=geometry_length,
-        geometry_width=geometry_width,
-        geometry_mesh_filename=geometry_mesh_filename,
-        geometry_mesh_origin_xyz=geometry_mesh_origin_xyz,
-        geometry_mesh_origin_rpy=geometry_mesh_origin_rpy,
-        geometry_mesh_tcp_xyz=geometry_mesh_tcp_xyz,
-        geometry_mesh_tcp_rpy=geometry_mesh_tcp_rpy,
-    ).moveit_cpp(
-        file_path=get_package_share_directory("micpp_manymove_planner") + "/config/moveit_cpp.yaml"
-    ).to_moveit_configs()
+    # RViz
+    rviz_config_file = (
+        get_package_share_directory("micpp_manymove_planner") + "/config/micpp_demo.rviz"
+    )
 
-    manymove_planner_node = Node(
-        package='micpp_manymove_planner',
-        executable='micpp_manymove_planner_node',
-        output='screen',
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
         parameters=[
-            moveit_configs.to_dict(),
-            {
-                'velocity_scaling_factor': velocity_scaling_factor,
-                'acceleration_scaling_factor': acceleration_scaling_factor,
-                'max_exec_retries': max_exec_retries,
-                'smoothing_type': smoothing_type,
-                'step_size': step_size,
-                'jump_threshold': jump_threshold,
-                'max_cartesian_speed': max_cartesian_speed,
-                'plan_number_target': plan_number_target,
-                'plan_number_limit': plan_number_limit,
-            }
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.planning_pipelines,
+            moveit_config.joint_limits,
         ],
     )
-
-    
-    # rviz with moveit configuration
-    rviz_config_file = PathJoinSubstitution([FindPackageShare(moveit_config_package_name), 'rviz', 'planner.rviz' if no_gui_ctrl.perform(context) == 'true' else 'moveit.rviz'])
-    rviz2_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', rviz_config_file],
-        parameters=[
-            {
-                'robot_description': moveit_configs.to_dict()['robot_description'],
-                'robot_description_semantic': moveit_configs.to_dict()['robot_description_semantic'],
-                'robot_description_kinematics': moveit_configs.to_dict()['robot_description_kinematics'],
-                'robot_description_planning': moveit_configs.to_dict()['robot_description_planning'],
-                'planning_pipelines': moveit_configs.to_dict()['planning_pipelines'],
-                'use_sim_time': use_sim_time
-            }
-        ],
-        condition=IfCondition(show_rviz),
-        remappings=[
-            ('/tf', 'tf'),
-            ('/tf_static', 'tf_static'),
-        ]
-    )
-
-    xyz = attach_xyz.perform(context)[1:-1].split(' ')
-    rpy = attach_rpy.perform(context)[1:-1].split(' ')
-    args = xyz + rpy + [attach_to.perform(context), '{}link_base'.format(prefix.perform(context))]
 
     # Static TF
     static_tf = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_transform_publisher',
-        output='screen',
-        arguments=args,
-        parameters=[{'use_sim_time': use_sim_time}],
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["--frame-id", "world", "--child-frame-id", "base_link"],
     )
 
-    robot_planner_node_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(PathJoinSubstitution([FindPackageShare('xarm_planner'), 'launch', '_robot_planner.launch.py'])),
-        condition=IfCondition(no_gui_ctrl),
-        launch_arguments={
-            'moveit_config_dump': moveit_configs.to_dict(),
-        }.items(),
+    # Publish TF
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[moveit_config.robot_description],
     )
 
-    return [
-        RegisterEventHandler(event_handler=OnProcessExit(
-            target_action=rviz2_node,
-            on_exit=[EmitEvent(event=Shutdown())]
-        )),
-        rviz2_node,
-        static_tf,
-        manymove_planner_node,
-        robot_planner_node_launch
-    ]
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("xarm_controller"),
+        "config",
+        "lite6_controllers.yaml",
+    )
 
-def generate_launch_description():
-    return LaunchDescription([
-        DeclareLaunchArgument('velocity_scaling_factor', default_value='0.5', description='Velocity scaling factor'),
-        DeclareLaunchArgument('acceleration_scaling_factor', default_value='0.5', description='Acceleration scaling factor'),
-        DeclareLaunchArgument('max_exec_retries', default_value='5', description='Maximum number of retries'),
-        DeclareLaunchArgument('smoothing_type', default_value='iterative_parabolic', description='Smoothing type'),
-        DeclareLaunchArgument('step_size', default_value='0.05', description='Step size'),
-        DeclareLaunchArgument('jump_threshold', default_value='0.0', description='Jump threshold'),
-        DeclareLaunchArgument('max_cartesian_speed', default_value='0.5', description='Max cartesian speed'),
-        DeclareLaunchArgument('plan_number_target', default_value='12', description='Plan number target'),
-        DeclareLaunchArgument('plan_number_limit', default_value='32', description='Plan number limit'),
-        OpaqueFunction(function=launch_setup)
-    ])
+
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[ros2_controllers_path],
+        remappings=[
+            ("/controller_manager/robot_description", "/robot_description"),
+        ],
+        output="both",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    arm_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["lite6_traj_controller", "-c", "/controller_manager"],
+    )
+
+    return LaunchDescription(
+        [
+            rviz_node,
+            static_tf,
+            robot_state_publisher,
+            run_move_group_node,
+            ros2_control_node,
+            joint_state_broadcaster_spawner,
+            arm_controller_spawner,
+        ]
+    )
+
+
+# import os
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+# from launch.actions import ExecuteProcess
+# from ament_index_python.packages import get_package_share_directory
+# from moveit_configs_utils import MoveItConfigsBuilder
+
+
+# def generate_launch_description():
+#     moveit_config = (
+#         MoveItConfigsBuilder("moveit_resources_panda")
+#         .robot_description(file_path="config/panda.urdf.xacro")
+#         .trajectory_execution(file_path="config/gripper_moveit_controllers.yaml")
+#         .moveit_cpp(
+#             file_path=get_package_share_directory("moveit2_tutorials")
+#             + "/config/moveit_cpp.yaml"
+#         )
+#         .to_moveit_configs()
+#     )
+#     # MoveItCpp demo executable
+#     moveit_cpp_node = Node(
+#         name="moveit_cpp_tutorial",
+#         package="moveit2_tutorials",
+#         executable="moveit_cpp_tutorial",
+#         output="screen",
+#         parameters=[moveit_config.to_dict()],
+#     )
+
+#     # RViz
+#     rviz_config_file = (
+#         get_package_share_directory("moveit2_tutorials")
+#         + "/launch/moveit_cpp_tutorial.rviz"
+#     )
+#     rviz_node = Node(
+#         package="rviz2",
+#         executable="rviz2",
+#         name="rviz2",
+#         output="log",
+#         arguments=["-d", rviz_config_file],
+#         parameters=[
+#             moveit_config.robot_description,
+#             moveit_config.robot_description_semantic,
+#         ],
+#     )
+
+#     # Static TF
+#     static_tf = Node(
+#         package="tf2_ros",
+#         executable="static_transform_publisher",
+#         name="static_transform_publisher",
+#         output="log",
+#         arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"],
+#     )
+
+#     # Publish TF
+#     robot_state_publisher = Node(
+#         package="robot_state_publisher",
+#         executable="robot_state_publisher",
+#         name="robot_state_publisher",
+#         output="both",
+#         parameters=[moveit_config.robot_description],
+#     )
+
+#     # ros2_control using FakeSystem as hardware
+#     ros2_controllers_path = os.path.join(
+#         get_package_share_directory("moveit_resources_panda_moveit_config"),
+#         "config",
+#         "ros2_controllers.yaml",
+#     )
+#     ros2_control_node = Node(
+#         package="controller_manager",
+#         executable="ros2_control_node",
+#         parameters=[moveit_config.robot_description, ros2_controllers_path],
+#         output="both",
+#     )
+
+#     # Load controllers
+#     load_controllers = []
+#     for controller in [
+#         "lite6_traj_controller",
+#         "joint_state_broadcaster",
+#     ]:
+#         load_controllers += [
+#             ExecuteProcess(
+#                 cmd=["ros2 run controller_manager spawner {}".format(controller)],
+#                 shell=True,
+#                 output="screen",
+#             )
+#         ]
+
+#     return LaunchDescription(
+#         [
+#             static_tf,
+#             robot_state_publisher,
+#             rviz_node,
+#             moveit_cpp_node,
+#             ros2_control_node,
+#         ]
+#         + load_controllers
+#     )
